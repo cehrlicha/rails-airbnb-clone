4369ddca9e7cde64e2c9b965d9046b09758e01c7
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-23 11:20:03 +0100

    Updated DB for users more info added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523095556) do
+ActiveRecord::Schema.define(version: 20170523101732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20170523095556) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "username"
+    t.string   "image"
+    t.string   "cover_picture"
+    t.string   "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
