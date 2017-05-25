1d518f36a882206aa831074a47b95f8887233a63
Gauthier Lagae <gauthier.lagae@gmail.com>
2017-05-25 13:36:25 +0100

    Restore show page buttons and style

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,7 @@
 class Friend < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   belongs_to :user
   has_many :sales, dependent: :destroy
   has_many :friend_availabilities, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524102540) do
+ActiveRecord::Schema.define(version: 20170525085149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20170524102540) do
     t.integer  "user_id"
     t.string   "photo"
     t.string   "last_name"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_friends_on_user_id", using: :btree
   end
 
