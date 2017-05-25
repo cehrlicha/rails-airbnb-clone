e735c8dff2ae9a86286b9cc3402ae7c2696b6504
Benoit Habfast <habfast.benoit@gmail.com>
2017-05-25 12:26:52 +0100

    on the go

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,4 +4,9 @@ class Sale < ApplicationRecord
   has_one :rating, dependent: :destroy
 
 
+  validates :start_daytime, presence: true
+  validates :end_daytime, presence: true
+  validates :buyer, presence: true
+  validates :friend, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   #resources :friends ,except: [:show]
   get '/friends/:id' , to: "friends#product", as: 'friend'
 
+  resources :sales, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
