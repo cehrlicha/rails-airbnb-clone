d1173468e99c884e915fd291c9051f5dba5df9a7
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-25 11:43:21 +0100

    Booking confirmation page

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,23 @@
 class SalesController < ApplicationController
 
+  def new
+    # byebug
+    @friend = Friend.find(params[:friend_id])
+    @date = params[:date]
+  end
   def create
-    @sale = Sale.new()
+    @date = params[:date]
+    @friend = Friend.find(params[:friend_id])
+
+    @sale = Sale.new(
+      start_daytime: @date,
+      end_daytime: @date,
+      friend: @friend,
+      buyer: current_user)
+
+    if @sale.save
+    else
+
+    end
   end
 end
